43ce84d3ea4ea9070a3934afc428d1dbc4cc4496
Andre V. Silva <andsilvadrcc@gmail.com>
2023-02-08 19:03:10 -0300

    update code search

--- python/getdata.py
+++ python/getdata.py
@@ -6,6 +6,8 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium_stealth import stealth ## https://pypi.org/project/selenium-stealth/
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
 options = webdriver.ChromeOptions()
 options.add_argument("start-maximized")
@@ -26,8 +28,12 @@ stealth(driver,
         fix_hairline=True,
         )
 
-driver.get('https://www.tiktok.com/en')
+driver.get('https://www.google.com')
 
-username = driver.find_element('xpath', '//*[@id="app"]/div[2]/div[2]/div[1]/div[1]/div/div[1]/div[1]/a[2]/h3').text
+time.sleep(3)
 
-print(username)
+search = driver.find_element_by_name("q")
+search.send_keys("house")
+search.send_keys(Keys.ENTER)
+
+time.sleep(5)
